0a887877a2e2cff904f4f2886ebfa2850450fd9b
CHAOS ADP <phobos24137@gmail.com>
2025-02-16 21:03:25 +0700

    pokemon

--- db-init/init.sql
+++ db-init/init.sql
@@ -10,11 +10,11 @@ CREATE TABLE IF NOT EXISTS pokemon
     weight  float        null,
     hp      int          null,
     attack  int          null,
-    defense int          null,
+    defense int          null
 );
 
-INSERT INTO pokemon.pokemon (name, id, type, height, weight, hp, attack, defense) VALUES ('bulbasaur', 1, 'grass', 7, 69, 45, 49, 49);
-INSERT INTO pokemon.pokemon (name, id, type, height, weight, hp, attack, defense) VALUES ('Pikachu', 22, 'Electric', 0.4, 6, 35, 55, 40);
-INSERT INTO pokemon.pokemon (name, id, type, height, weight, hp, attack, defense) VALUES ('Charizard', 23, 'Fire/Flying', 1.7, 90.5, 78, 84, 78);
-INSERT INTO pokemon.pokemon (name, id, type, height, weight, hp, attack, defense) VALUES ('Squirtle', 25, 'Water', 0.5, 9, 44, 48, 65);
-INSERT INTO pokemon.pokemon (name, id, type, height, weight, hp, attack, defense) VALUES ('Jigglypuff', 26, 'Normal/Fairy', 0.5, 5.5, 115, 45, 20);
+INSERT INTO pokemon (name, id, type, height, weight, hp, attack, defense) VALUES ('bulbasaur', 1, 'grass', 7, 69, 45, 49, 49);
+INSERT INTO pokemon (name, id, type, height, weight, hp, attack, defense) VALUES ('Pikachu', 22, 'Electric', 0.4, 6, 35, 55, 40);
+INSERT INTO pokemon (name, id, type, height, weight, hp, attack, defense) VALUES ('Charizard', 23, 'Fire/Flying', 1.7, 90.5, 78, 84, 78);
+INSERT INTO pokemon (name, id, type, height, weight, hp, attack, defense) VALUES ('Squirtle', 25, 'Water', 0.5, 9, 44, 48, 65);
+INSERT INTO pokemon (name, id, type, height, weight, hp, attack, defense) VALUES ('Jigglypuff', 26, 'Normal/Fairy', 0.5, 5.5, 115, 45, 20);
